7dc5ad9f24b9fa4e4f4fd93d8f8ec7dd4f087bf1
suriwong <@hhhhh>
2017-07-05 17:35:08 +0700

    initial upgrade angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule }   from '@angular/router';
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
-
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -15,8 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MdButtonModule,
-    MdCheckboxModule
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
